c4d42721f4d3d8a4dab01cc2e43c8de2ff56c7c3
Poorna chary <poornachary1@gmail.com>
2026-01-30 00:40:59 +0530

    Create route for goals and handle http methods

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 5000;
 console.log(process.env.PORT)
 const app = express();
 
-
+app.use('/api/goals', require('./routes/goalRoutes'))
 app.listen(port, () => console.log(`Server started on ${port}`))
