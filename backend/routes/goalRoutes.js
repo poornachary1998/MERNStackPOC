930a0ecc3a2e3bb574fcaadd7d82061b007c4144
Poorna chary <poornachary1@gmail.com>
2026-01-30 11:57:36 +0530

    chaining routes having same path.

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 const {getGoals, postGoals, putGoals, deleteGoals} = require('../controllers/goalControllerr');
+// connecting controller functions making code clean;
+router.route('/').get(getGoals).post(postGoals);
+router.route('/:id').delete(deleteGoals).put(putGoals);
 
-router.get('/' , getGoals);
+// router.get('/' , getGoals);
 
-router.post('/', postGoals)
+// router.post('/', postGoals)
  
-router.put('/:id', putGoals)
+// router.put('/:id', putGoals)
 
-router.delete('/:id', deleteGoals)
+// router.delete('/:id', deleteGoals)
 
 module.exports = router;
