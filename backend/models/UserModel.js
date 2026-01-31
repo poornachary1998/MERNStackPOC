577b5b20ac9399256a2d0a58325fc6c46f756213
Poorna chary <poornachary1@gmail.com>
2026-01-31 23:08:19 +0530

    Created user model and addded to goal model for user specific goals

---
+++ backend/models/UserModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    user:{
+        type: String,
+        required: [true, "This field is required."]
+    },
+    email:{
+        type: String,
+        required: [true, "This field is required."]
+    },
+    password:{
+        type: String,
+        required: [true, "This field is required."]
+    }
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema);
