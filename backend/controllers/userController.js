c2502003bfde2ff298ad983b66fa6d63e1ae814f
Poorna chary <poornachary1@gmail.com>
2026-02-02 01:14:52 +0530

    User login controller logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,39 +11,53 @@ const registerUser = AsyncHandler(async (req, res) => {
 
     // Validate required fields and *return* the error response to stop execution
     if (!name || !email || !password) {
-         res.status(400)
-      throw new Error({ message: 'Enter the required fields' });
+        res.status(400)
+        throw new Error({ message: 'Enter the required fields' });
     }
 
-   const userExist = await User.findOne({ email });
-if (userExist) {
-  return res.status(400).json({ message: 'User already exists' });
-}
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     //create user
-    const user  = await User.create({
+    const user = await User.create({
         name,
         email,
         password: hashedPassword
     })
-    if (user){
-    // Placeholder success response (use 201 Created for resources
-    res.status(201).json({
-        _id: user.id,
-        name: user.name,
-        email: user.email
-     });}
+    if (user) {
+        // Placeholder success response (use 201 Created for resources
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        });
+    }
     else {
-         res.status(400);
+        res.status(400);
         throw new Error("Invalid user details");
     }
 })
 
 const loginUser = AsyncHandler(async (req, res) => {
-    res.json({ message: 'user login data' })
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }else{
+        res.status(400)
+    throw new Error ("Invalid credentials")
+    }
 })
 
 const getMyAccount = AsyncHandler(async (req, res) => {
